1e33118280c382fe5e56887646dc68dc9d9a62d5
Roc.Tian <pftian@yaduo.com>
2017-04-05 19:20:12 +0800

    修复bug

--- TPFControllerAllocTrace.podspec
+++ TPFControllerAllocTrace.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'TPFControllerAllocTrace'
-  s.version          = '0.1.0'
+  s.version          = '0.1.1'
   s.summary          = 'A short description of TPFControllerAllocTrace.'
 
 # This description is used to generate tags and improve search results.
--- TPFControllerAllocTrace/Classes/TPFControllerAllocTrace.m
+++ TPFControllerAllocTrace/Classes/TPFControllerAllocTrace.m
@@ -18,7 +18,7 @@
 @implementation TPFControllerAllocTrace
 
 +(instancetype)sharedControllerAllocTrace{
-
+    
     static TPFControllerAllocTrace *controllerAllocTrace;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
@@ -27,33 +27,33 @@
     return controllerAllocTrace;
 }
 -(instancetype)init{
-
+    
     self = [super init];
     if(self){
-    
+        
         _controllersDictionary = [[NSMutableDictionary alloc] init];
         [self initTraceMethod];
     }
     return self;
 }
 -(void)initTraceMethod{
-
+    
     NSError *error;
     __weak typeof(self) weakSlef = self;
     [UIViewController aspect_hookSelector:@selector(dismissViewControllerAnimated:completion:)
-                             withOptions:AspectPositionAfter
-                              usingBlock:^(id <AspectInfo> aspectInfo){
-                                  UIViewController *viewController = aspectInfo.instance;
-                                  NSString *className = [NSString stringWithFormat:@"%s",object_getClassName(viewController)];
-                                  [weakSlef.controllersDictionary setValue:[self startTimer:className] forKey:className];
-                              }
-                                   error:&error];
+                              withOptions:AspectPositionAfter
+                               usingBlock:^(id <AspectInfo> aspectInfo){
+                                   UIViewController *viewController = aspectInfo.instance;
+                                   NSString *className = [self getClassName:viewController];
+                                   [weakSlef.controllersDictionary setValue:[self startTimer:className] forKey:className];
+                               }
+                                    error:&error];
     
     SEL sel = NSSelectorFromString(@"dealloc");
     [UIViewController aspect_hookSelector:sel withOptions:AspectPositionBefore usingBlock:^(id<AspectInfo> aspectInfo) {
         
         UIViewController *viewController = aspectInfo.instance;
-        NSString *className = [NSString stringWithFormat:@"%s",object_getClassName(viewController)];
+        NSString *className = [self getClassName:viewController];
         [weakSlef invalidateTimer:className];
         
         NSLog(@"%@ dealloc",className);
@@ -65,34 +65,51 @@
         UINavigationController *navigationController = aspectInfo.instance;
         NSArray *viewControllers = [navigationController viewControllers];
         UIViewController  *viewController = viewControllers.lastObject;
-        NSString *className = [NSString stringWithFormat:@"%s",object_getClassName(viewController)];
-       [weakSlef.controllersDictionary setValue:[self startTimer:className] forKey:className];
+        NSString *className = [self getClassName:viewController];
+        [weakSlef.controllersDictionary setValue:[self startTimer:className] forKey:className];
         
     } error:NULL];
-
+    
+}
+-(NSString *)getClassName:(id)object{
+    
+    NSString *className = [NSString stringWithUTF8String:object_getClassName(object)];
+    NSArray *nameArrays = [className componentsSeparatedByString:@"_"];
+    if(nameArrays.count==1){
+        
+        return nameArrays[0];
+    }
+    else if(nameArrays.count==2){
+        
+        return nameArrays[1];
+    }
+    else
+        return @"Unknow";
 }
 -(NSTimer *)startTimer:(NSString *)className{
-
-    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:2.0f target:self selector:@selector(noAllocWaring:) userInfo:@{@"className":className} repeats:NO];
+    
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:10.0f target:self selector:@selector(noAllocWaring:) userInfo:@{@"className":className} repeats:NO];
     
     return timer;
 }
 -(void)invalidateTimer:(NSString *)className{
-
+    
     NSTimer *timer = [self.controllersDictionary valueForKey:className];
     [timer invalidate];
     [self.controllersDictionary removeObjectForKey:className];
     timer = nil;
 }
 -(void)noAllocWaring:(NSTimer *)timer{
-
+    
     NSDictionary *userInfo = timer.userInfo;
     NSString *className = [userInfo valueForKey:@"className"];
-    NSString *warningMessage = [NSString stringWithFormat:@"%@ 没有被释放，请检查是否发生了泄漏",className];
-    
-    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:nil message:warningMessage delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
-    [alertView show];
-    
-    [self invalidateTimer:className];
+    if([self.controllersDictionary valueForKey:className]){
+        NSString *warningMessage = [NSString stringWithFormat:@"%@ 没有被释放，请检查是否发生了泄漏，循环引用。测试人员请注意，督促相关人员修复",className];
+        
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:nil message:warningMessage delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
+        [alertView show];
+        
+        [self invalidateTimer:className];
+    }
 }
 @end
