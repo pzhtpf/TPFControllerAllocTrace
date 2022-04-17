6672d3fb9affe3171f7357e3aa203ead10755186
pengfei tian <389744841@qq.com>
2022-04-18 00:08:19 +0800

    feat: 判断图中是否有环路

--- TPFControllerAllocTrace/Classes/TPFCycleRetainManager.m
+++ TPFControllerAllocTrace/Classes/TPFCycleRetainManager.m
@@ -29,13 +29,14 @@
 - (void)analyse:(id)object {
     NSMutableDictionary *vertexs = [[NSMutableDictionary alloc] init];
     TPFLinkedList *head = [self findInstanceObject:object vertexs:vertexs];
+    head.className = NSStringFromClass([object class]);
     head.propertyName = @"headViewController";
     [self graphIfExistCycle:head vertexs:vertexs];
 }
 
 - (TPFLinkedList *)findInstanceObject:(id)object vertexs:(NSMutableDictionary *)vertexs {
     TPFLinkedList *node = [[TPFLinkedList alloc] init];
-    node.className = NSStringFromClass([object class]);
+//    node.className = NSStringFromClass([object class]);
     node.address = [NSString stringWithFormat:@"%p", object];
     [vertexs setObject:node forKey:node.address];
     
@@ -53,6 +54,9 @@
         
         NSString* propertyAttributes = [NSString stringWithUTF8String:property_getAttributes(property)];
         NSLog(@"propertyAttributes: %@", propertyAttributes);
+//        if ([propertyAttributes containsString:@"UIViewController"]) {
+//
+//        }
         NSArray* splitPropertyAttributes = [propertyAttributes componentsSeparatedByString:@"\""];
         if ([splitPropertyAttributes count] >= 2)
         {
@@ -66,16 +70,21 @@
                 NSArray *modifierArray = [modifier componentsSeparatedByString:@","];
                 if ([self isStrongRef:modifierArray]) {
                     NSString *addressKey = [NSString stringWithFormat:@"%p", propertyObject];
+                    TPFLinkedList *childNode;
                     if ([vertexs objectForKey:addressKey] == nil) {
-                     TPFLinkedList *childNode = [self findInstanceObject:propertyObject vertexs:vertexs];
-                        childNode.propertyName = propertyName;
-                        if (!head) {
-                            head = childNode;
-                            node.childrens = head;
-                            
-                        } else {
-                            head.next = childNode;
-                        }
+                        childNode = [self findInstanceObject:propertyObject vertexs:vertexs];
+                    } else {
+                        childNode = [[TPFLinkedList alloc] init];
+                        childNode.address = addressKey;
+                    }
+                    childNode.propertyName = propertyName;
+                    childNode.className = className;
+                    if (!head) {
+                        head = childNode;
+                        node.childrens = head;
+                        
+                    } else {
+                        head.next = childNode;
                     }
                 }
             }
@@ -98,7 +107,47 @@
 }
 
 - (void)graphIfExistCycle:(TPFLinkedList *)head vertexs:(NSMutableDictionary *)vertexs {
+    NSMutableArray *cyclePath = [[NSMutableArray alloc] init];
+    [self dfs:head path:[NSMutableArray new] visited:[NSMutableDictionary new] cyclePath:cyclePath];
+    NSLog(@"cyclePathCount:%ld", cyclePath.count);
+}
+
+- (void)dfs:(TPFLinkedList *)head path:(NSMutableArray *)path visited:(NSMutableDictionary *)visited cyclePath:(NSMutableArray *)cyclePath {
+    if ([visited objectForKey:head.address]) { /* 说明存在环 */
+        [path addObject:head];
+        [path addObject:[visited objectForKey:head.address]];
+        NSMutableArray *serializeArray = [self serializeCyclePath:path];
+        [cyclePath addObject:[serializeArray description]];
+        [path removeLastObject];
+        [path removeLastObject];
+        return;
+    }
     
+    [path addObject:head];
+    [visited setObject:head forKey:head.address];
+    
+    TPFLinkedList *childHead = head.childrens;
+    while (childHead) {
+//        [path addObject:childHead];
+//        [visited setObject:childHead forKey:childHead.address];
+        [self dfs:childHead path:path visited:visited cyclePath:cyclePath];
+//        [path removeLastObject];
+//        [visited removeObjectForKey:childHead.address];
+        childHead = childHead.next;
+    }
+    
+    [path removeLastObject];
+    [visited removeObjectForKey:head.address];
+}
+
+- (NSMutableArray *)serializeCyclePath:(NSMutableArray *)cyclePath {
+    NSMutableArray *serializeArray = [[NSMutableArray alloc] init];
+    for (int i = 0; i < cyclePath.count; i++) {
+        TPFLinkedList *node = cyclePath[i];
+        NSString *string = [NSString stringWithFormat:@"ClassName=%@;PropertyName=%@;Address=%@",node.className,node.propertyName,node.address];
+        [serializeArray addObject:string];
+    }
+    return serializeArray;
 }
 
 @end
